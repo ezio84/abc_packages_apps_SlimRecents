03736b6e67b89e549acde59bf23aa1815c6a8983
SpiritCroc <spiritcroc@gmail.com>
2017-03-11 14:21:24 +0100

    Fix blinking cards on notifyDataSetChanged()
    Cards would animate semi-transparent and back after removing
    other cards, but sometimes also when actually nothing changed
    (but notifyDataSetChanged() called).
    
    Add a custom ItemAnimator to still have the transparency thing
    going when expanding/collapsing cards.
    
    Change-Id: I14bcc9408db1b71f47a436cd46ea5a8a4a52ce2d

--- src/com/android/systemui/slimrecent/RecentController.java
+++ src/com/android/systemui/slimrecent/RecentController.java
@@ -51,6 +51,7 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -118,6 +119,8 @@ public class RecentController implements RecentPanelView.OnExitListener,
     private boolean mIsToggled;
     private boolean mIsPreloaded;
     private boolean mIsUserSetup;
+
+    private boolean mExpandAnimation = false;
 
     // The different views we need.
     private ViewGroup mParentView;
@@ -203,6 +206,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                 (RecyclerView) mRecentContainer.findViewById(R.id.recent_list);
 
         mCardRecyclerView.setHasFixedSize(true);
+        mCardRecyclerView.setItemAnimator(mItemAnimator);
 
         mEmptyRecentView =
                 (ImageView) mRecentContainer.findViewById(R.id.empty_recent);
@@ -1224,4 +1228,21 @@ public class RecentController implements RecentPanelView.OnExitListener,
         public void onConfigurationChanged(Configuration newConfig) {
         }
     }
+
+    public void onStartExpandAnimation() {
+        mExpandAnimation = true;
+    }
+
+    private DefaultItemAnimator mItemAnimator = new DefaultItemAnimator() {
+        @Override
+        public boolean canReuseUpdatedViewHolder(RecyclerView.ViewHolder viewHolder) {
+            // Similar to SimpleItemAnimator.setSupportsChangeAnimations(mExpandAnimation)
+            if (mExpandAnimation) {
+                mExpandAnimation = false;
+                return super.canReuseUpdatedViewHolder(viewHolder);
+            }
+            // Returning true means we don't support change animations here
+            return true;
+        }
+    };
 }
