f731c781da7d5c3d91a87052a1ee55cef119c35c
Lars Greiss <kufikugel@googlemail.com>
2017-03-24 10:26:42 +0100

    Slim Recents [1/2]

---
+++ Android.mk
@@ -0,0 +1,16 @@
+#LOCAL_PATH:= $(call my-dir)
+#
+#include $(CLEAR_VARS)
+#
+#LOCAL_JAVA_LIBRARIES :=
+#
+#LOCAL_STATIC_JAVA_LIBRARIES :=
+#
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#
+#LOCAL_MODULE_TAGS := optional
+#
+#LOCAL_MODULE :=
+#
+#include $(BUILD_STATIC_JAVA_LIBRARY)
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# android_packages_apps_SlimRecents
+
+Alternative Recents
+
