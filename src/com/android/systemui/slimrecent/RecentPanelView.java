8ae666e147f4d4c0c2e94ba746d9f41855cfe625
ezio84 <brabus84@gmail.com>
2018-04-04 23:51:18 +0200

    Track info: show duration before artist and title
    duration can be an useful info, it's a pity when it gets cut
    because the track title is too long

--- src/com/android/systemui/slimrecent/RecentPanelView.java
+++ src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -1377,7 +1377,7 @@ public class RecentPanelView {
             }
         }
         if (charSequence != null && lenghtInfo != null) {
-            return (charSequence + " - " + lenghtInfo).toString();
+            return (lenghtInfo + " | " + charSequence).toString();
         } else if (charSequence != null) {
             return charSequence.toString();
         }
